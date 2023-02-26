108c88eb4fe2568a32619564d7df84171b4ca517
Wiktor Lukasik <wiktor@lukasik.org>
2023-02-26 13:19:56 -0800

    Splitting IPFS upload into two modes - local and gateway

--- script/upload2ipfs.py
+++ script/upload2ipfs.py
@@ -2,14 +2,56 @@
 
 import ipfshttpclient
 import sys
+import requests
+import argparse
+from dotenv import load_dotenv
+import os
+
+
+load_dotenv()
+app_key = os.getenv("IPFS_PROJECT_ID")
+secret_key = os.getenv("IPFS_SECRET_KEY")
+
+
+parser = argparse.ArgumentParser()
+parser.add_argument("--mode", "-m", type=str)
+parser.add_argument("--file", "-f", type=str)
+args = parser.parse_args()
+
+
+def upload_via_local_node(file_path):
+    client = ipfshttpclient.connect('/ip4/127.0.0.1/tcp/5001')
+
+    res = client.add(file_path)
+    print("Address: " + str(res['Hash']))
+
+
+def upload_via_gateway(file_path):
+
+    url = 'https://ipfs.infura.io:5001/api/v0/add'
+
+    files = {
+        'file': file_path
+    }
+
+    response = requests.post(url, files=files, auth=(app_key, secret_key))
+
+    # Print the IPFS hash of the uploaded file
+    print("Address: " + str(response.text))
 
 
 if __name__ == "__main__":
-    if len(sys.argv) < 2:
-        print("Valid path required...")
+    if args.file is None or args.mode is None:
+        print("Valid path and run mode are required...")
         sys.exit(1)
 
-    client = ipfshttpclient.connect('/ip4/127.0.0.1/tcp/5001')
-    file_path = sys.argv[1]
-    res = client.add(file_path)
-    print("Address: " + res['Hash'])
+    file_path = args.file
+    mode = args.mode
+
+    if mode == "local":
+        upload_via_local_node(file_path)
+    elif mode == "gw":
+        upload_via_gateway(file_path)
+    else:
+        print("Invalid mode")
+        sys.exit(1)
